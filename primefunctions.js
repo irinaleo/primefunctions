a758f2ee7ff0a933bdce3379c341227a208c3423
irina.bejan <irina.bejan@wsu.edu>
2020-11-01 01:45:45 -0700

    Finished cumulativeSum.

--- primefunctions.js
+++ primefunctions.js
@@ -20,18 +20,45 @@ function primeGen(threshold) {
   let array = Array.from({ length: threshold - 2 }, (_, i) => i + 2);
 
   // remove all values that are a multiple of the current value
-  let i = 0;
-  let c = array[i];
-  while (c <= array.length) {
+  _.each(array, function (element, index) {
+    const c = array[index];
     array = _.reject(array, function (num) {
-      if (c !== num) { return num % c === 0; }});
-    c = array[i++];
-  }
+      if (c !== num) { return num % c === 0; }
+      return 0;
+    });
+  });
+
   return array;
+}
+
+/*
+2) cumulativeSum: This function takes a list of numbers, and
+returns the cumulative sum of these numbers.
+
+e.g. cumulativeSum([1, 2, 3, 4]) => [1, 3, 6, 10]
+
+For each value in the inserted array, it should be the sum of
+it’s preceding values in the input array. So, position 2 is the
+sum of positions 0-2, which is 1+2+3 = 6 in the above example.
+*/
+
+function cumulativeSum(array) {
+  // create a new array and int to hold the sum
+  const newArray = []; let
+    sum = 0;
+
+  // for each value in array, sum the total and set it as the value for the new array
+  _.each(array, function (num, index) {
+    sum += array[index];
+    newArray[index] = sum;
+  });
+
+  return newArray;
 }
 
 function main() {
   console.log(primeGen(10));
+  console.log(cumulativeSum([1, 2, 3, 4, 5, 6]));
 }
 
 main();
