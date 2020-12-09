01dae0d49e7a53a32ba6f4452de2cd3717ec631f
irina.bejan <irina.bejan@wsu.edu>
2020-12-09 13:09:42 -0800

    Finished tests for cumulativeSum and maxPrimeSum.

--- testCorrectness.js
+++ testCorrectness.js
@@ -1,4 +1,4 @@
-/* global primeGen */
+/* global primeGen, cumulativeSum, maxPrimeSum */
 
 describe('Test for Correctness', function () {
   describe('primeGen', function () {
@@ -7,6 +7,24 @@ describe('Test for Correctness', function () {
     });
     it('primeGen(20) = [2, 3, 5, 7, 11, 13, 17, 19]', function () {
       chai.expect(primeGen(20)).to.deep.equal([2, 3, 5, 7, 11, 13, 17, 19]);
+    });
+  });
+
+  describe('cumulativeSum', function () {
+    it('cumulativeSum([1, 2, 3, 4]) = [1, 3, 6, 10]', function () {
+      chai.expect(cumulativeSum([1, 2, 3, 4])).to.deep.equal([1, 3, 6, 10]);
+    });
+    it('cumulativeSum([10, 11, 12, 13, 14]) = [10, 21, 33, 46, 60]', function () {
+      chai.expect(cumulativeSum([10, 11, 12, 13, 14])).to.deep.equal([10, 21, 33, 46, 60]);
+    });
+  });
+
+  describe('maxPrimeSum', function () {
+    it('maxPrimeSum(100) = [41, 6]', function () {
+      chai.expect(maxPrimeSum(100)).to.deep.equal([41, 6]);
+    });
+    it('maxPrimeSum(1000) = [953, 21]', function () {
+      chai.expect(maxPrimeSum(1000)).to.deep.equal([953, 21]);
     });
   });
 });
