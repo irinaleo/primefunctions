a3de6e1926054510f8b32215b0c71d97c5ce20b0
irina.bejan <irina.bejan@wsu.edu>
2020-12-09 15:46:36 -0800

    Finished first second test for run-time.

--- testPerformance.js
+++ testPerformance.js
@@ -6,5 +6,9 @@ describe('Test for Performance', function () {
       this.timeout(50);
       chai.expect(maxPrimeSum(10000)).to.deep.equal([9521, 65]);
     });
+    it('maxPrimeSum(100000) should take less than 3000ms', function () {
+      this.timeout(3000);
+      chai.expect(maxPrimeSum(100000)).to.deep.equal([92951, 183]);
+    });
   });
 });
